a3cd5ffeb4545a535dedf71754d9719c7502ea83
diaa <diaa2442000@gmail.com>
2023-09-06 20:26:15 +0300

    change the icon in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaDribbbleSquare, FaFacebookSquare, FaGithubSquare, FaInstagram, FaTwitterSquare } from 'react-icons/fa';
+import { FaDribbble, FaFacebook, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -8,11 +8,11 @@ const Footer = () => {
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] uppercase'>react.</h1>
                 <p className='py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, dolor quos. Amet sint distinctio, impedit, necessitatibus consequuntur nihil reprehenderit enim voluptate placeat dolor id soluta?</p>
                 <div className='flex justify-start gap-2 w-[40%] my-2'>
-                    <FaFacebookSquare size={30} />
+                    <FaFacebook size={30} />
                     <FaInstagram size={30} />
-                    <FaTwitterSquare size={30} />
-                    <FaGithubSquare size={30} />
-                    <FaDribbbleSquare size={30} />
+                    <FaTwitter size={30} />
+                    <FaGithub size={30} />
+                    <FaDribbble size={30} />
                 </div>
             </div>
             <div className='lg:col-span-2 flex justify-between'>
